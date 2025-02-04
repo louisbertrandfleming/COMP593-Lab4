2d0646d4b8bb2febe2d7b03c1c39d4b2aa0c41fd
Louis Bertrand <louis.bertrand@flemingcollege.ca>
2025-02-04 09:25:12 -0500

    Copy template to start script.

---
+++ log_analysis.py
@@ -0,0 +1,56 @@
+'''log_analysis.py - Analyze a network gateway log file using regular expressions.
+COMP 593 Scripting Applications - Winter 2025 (Week 5)
+Louis Bertrand <louis.bertrand@flemingcollege.ca>
+
+Usage:
+    python log_analysis <logfile>
+    where <logfile> is the file name or path to the log file.
+
+### STUDENTS: PLEASE ADD THE STANDARD ACADEMIC INTEGRITY STATEMENT.###
+# This program is strictly my own work. Any material beyond course learning
+# materials that is taken from the Web or other sources is properly cited,
+# giving credit to the original author(s).
+
+'''
+
+def main():
+    log_file = get_log_file_path_from_cmd_line()
+
+# TODO: Step 3
+def get_log_file_path_from_cmd_line():
+    return
+
+# TODO: Steps 4-7
+def filter_log_by_regex(log_file, regex, ignore_case=True, print_summary=False, print_records=False):
+    """Gets a list of records in a log file that match a specified regex.
+
+    Args:
+        log_file (str): Path of the log file
+        regex (str): Regex filter
+        ignore_case (bool, optional): Enable case insensitive regex matching. Defaults to True.
+        print_summary (bool, optional): Enable printing summary of results. Defaults to False.
+        print_records (bool, optional): Enable printing all records that match the regex. Defaults to False.
+
+    Returns:
+        (list, list): List of records that match regex, List of tuples of captured data
+    """
+    return
+
+# TODO: Step 8
+def tally_port_traffic(log_file):
+    return
+
+# TODO: Step 9
+def generate_port_traffic_report(log_file, port_number):
+    return
+
+# TODO: Step 11
+def generate_invalid_user_report(log_file):
+    return
+
+# TODO: Step 12
+def generate_source_ip_log(log_file, ip_address):
+    return
+
+if __name__ == '__main__':
+    main()
